38000390123adf4a3be43adb210324f2ade4b5c1
Jamiil007 <oussama.jamil.11@gmail.com>
2026-02-28 23:50:33 +0000

    Fix views imports for ALX checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
-from .models import Book, Library
+
+from .models import Book
+from .models import Library
 
 
-# ✅ Function-Based View
+# Function-based view
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
-# ✅ Class-Based View
+# Class-based view
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
